544e826e0c12b9aeb9386f0afe18fd043917526a
Adrien Maranville <adrien@maranville.io>
2022-07-22 12:24:33 -0600

    🚀 Integrates SNS

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -4,6 +4,20 @@ const aws4 = require('aws4')
 const URL = require('url')
 const http = require('axios')
 const mode = process.env.TEST_MODE
+const EventBridge = require('aws-sdk/clients/eventbridge')
+
+const viaEventBridge = async (busName, source, detailType, detail) => {
+    const eventBridge = new EventBridge()
+    await eventBridge.putEvents({
+        Entries: [{
+            Source: source,
+            DetailType: detailType,
+            Detail: JSON.stringify(detail),
+            EventBusName: busName
+        }]
+    }).promise()
+}
+
 
 const viaHandler = async (event, functionName) => {
     const handler = require(`${APP_ROOT}/functions/${functionName}`).handler
@@ -122,10 +136,10 @@ const we_invoke_notify_restaurant = async (event) => {
     if (mode === 'handler') {
         await viaHandler(event, 'notify-restaurant')
     } else {
-        throw new Error('not supported')
+        const busName = process.env.bus_name
+        await viaEventBridge(busName, event.source, event['detail-type'], event.detail)
     }
 }
-
 
 module.exports = {
     we_invoke_get_index,
